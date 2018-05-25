a4bdabea187ab37554e8d7a53c7f16edd34e57be
Vladislav <vladislav.s@shop-loyalty.ru>
2018-05-25 23:58:40 +0500

    Initial commit

---
+++ IocExample/DependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace IocExample
+{
+    public class DependencyResolver
+    {
+        private readonly Dictionary<Type, Type> types;
+        private readonly Dictionary<Type, object> objects;
+
+        public DependencyResolver()
+        {
+            types = new Dictionary<Type, Type>();
+            objects = new Dictionary<Type, object>();
+        }
+        
+        public void Bind<TClass, TImplementation>()
+        {
+            types[typeof(TClass)] = typeof(TImplementation);
+        }
+        public void Bind<TClass, TImplementation>(TImplementation instance)
+        {
+            objects[typeof(TClass)] = instance;
+        }
+        public T Get<T>()
+        {
+            return (T)Get(typeof(T));
+        }
+        private object Get(Type type)
+        {
+            if (objects.ContainsKey(type))
+                return objects[type];
+            Type implementation = types[type];
+            ConstructorInfo constructor = Utils.GetSingleConstructor(implementation);
+            ParameterInfo[] constructorParameters = constructor.GetParameters();
+            if (constructorParameters.Length == 0)
+                return Utils.CreateInstance(implementation);
+            List<object> parameters = new List<object>(constructorParameters.Length);
+            foreach (ParameterInfo parameterInfo in constructorParameters)
+                parameters.Add(Get(parameterInfo.ParameterType));
+            return Utils.CreateInstance(implementation, parameters);
+        }
+    }
+}
---
+++ IocExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using IocExample.Classes;
+using Ninject;
+
+namespace IocExample
+{
+    class Program
+    {
+//        static void Main(string[] args)
+//        {
+//            var logger = new ConsoleLogger();
+//            var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+//            var createUserHandler = new CreateUserHandler(
+//                new UserService(
+//                    new QueryExecutor(sqlConnectionFactory), 
+//                    new CommandExecutor(sqlConnectionFactory), 
+//                    new CacheService(logger, 
+//                        new RestClient("API KEY"))), logger);
+//
+//            createUserHandler.Handle();
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            var kernel = new StandardKernel();
+//
+//            kernel.Bind<ILogger>().To<ConsoleLogger>();
+//            kernel.Bind<IConnectionFactory>()
+//                .ToConstructor(key => new SqlConnectionFactory("SQL Connection", key.Inject<ILogger>()))
+//                .InSingletonScope();
+//            kernel.Bind<CreateUserHandler>().To<CreateUserHandler>();
+//            kernel.Bind<UserService>().To<UserService>();
+//            kernel.Bind<QueryExecutor>().To<QueryExecutor>();
+//            kernel.Bind<CommandExecutor>().To<CommandExecutor>();
+//            kernel.Bind<CacheService>().To<CacheService>();
+//            kernel.Bind<RestClient>().ToConstructor(key => new RestClient("API_KEY"));
+//            kernel.Get<CreateUserHandler>().Handle();
+//        }
+
+        static void Main(string[] args)
+        {
+            DependencyResolver resolver = new DependencyResolver();
+        
+            resolver.Bind<ILogger, ConsoleLogger>();
+            resolver.Bind<IConnectionFactory, SqlConnectionFactory>(
+                new SqlConnectionFactory("SQL Connection", resolver.Get<ILogger>()));
+            resolver.Bind<CreateUserHandler, CreateUserHandler>();            resolver.Bind<UserService, UserService>();
+            resolver.Bind<QueryExecutor, QueryExecutor>();
+            resolver.Bind<CommandExecutor, CommandExecutor>();
+            resolver.Bind<CacheService, CacheService>();
+            resolver.Bind<RestClient, RestClient>(new RestClient("API_KEY"));
+
+            resolver.Get<CreateUserHandler>().Handle();
+        }
+    }
+}
